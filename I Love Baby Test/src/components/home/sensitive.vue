d8b6d9fe822c33e4792167028011756a4928938a
lxl <1034406653@qq.com>
2018-08-10 09:32:24 +0800

    富文本编辑器

--- I Love Baby Test/src/components/home/sensitive.vue
+++ I Love Baby Test/src/components/home/sensitive.vue
@@ -1,125 +1,153 @@
 <template>
-  <div class="template-content sensitive-box">
-    <div class="header">
-      <div class="title">Sensitive words</div>
-    </div>
-    <div class="sensitive-main">
-      <div class="selectLi"> <input type="text" placeholder="Please enter the sensitive word" v-model="inputText" /> <span class="search-sensitive-btn" @click="searchSensitive">
+	<div class="template-content sensitive-box">
+		<div class="header">
+			<div class="title">Sensitive words</div>
+		</div>
+		<div class="sensitive-main">
+			<div class="selectLi"> <input type="text" placeholder="Please enter the sensitive word" v-model="inputText" /> <span class="search-sensitive-btn" @click="searchSensitive">
       		<img src="../../assets/web/search.png">
       	</span> <span class="add-sensitive-btn" @click="addSensitive">
       		<img src="../../assets/web/icon-add.png">
       	</span> </div>
-      <div class="textbox" v-if="textBoxShow"> <span v-for="item in searchWordData"><span>{{item.word}}</span><img src="../../assets/web/del-btn.png" @click="delSensitiveWord(item.wordId,item.word)" /></span>
-      </div>
-      <div class="article-hint">{{hint}}</div>
-    </div>
-    <div id="delWordModal"></div>
-  </div>
+			<div class="textbox" v-if="textBoxShow"> <span v-for="item in searchWordData"><span>{{item.word}}</span><img src="../../assets/web/del-btn.png" @click="delSensitiveWord(item.wordId,item.word)" /></span>
+			</div>
+			<div class="article-hint">{{hint}}</div>
+		</div>
+		<div id="delWordModal"></div>
+		<!--测试编辑框-->
+		<editor id="editor_id" height="500px" width="700px" :content="editorText" :afterChange="afterChange()" pluginsPath="/static/kindeditor/plugins/" :loadStyleMode="false" @on-content-change="onContentChange"></editor>
+		<!--测试编辑框-->
+	</div>
 </template>
 <script>
-  import { Modal } from '../../../static/js/modal.js'
-  var delWordModal = "";
-  export default {
-    data() {
-      return {
-        liangliang: sessionStorage.getItem("liangliang").substring(0, sessionStorage.getItem("liangliang").length - 10),
-        baseUrl: sessionStorage.getItem("baseUrl"),
-        inputText: "",
-        textBoxShow: false,
-        searchWordData: [],
-        hint: "",
-      }
-    },
-    methods: {
-      searchSensitive() {
-        var that = this;
-        if(this.inputText) {
-          $.ajax({
-            url: that.baseUrl + "searchSensitiveWord",
-            type: "POST",
-            data: "{'word':'" + that.inputText + "'}",
-            headers: {
-              'Authorization': that.liangliang,
-            },
-            dataType: "json",
-            contentType: "application/json",
-            success: function(rex) {
-              if(rex.code == 200) {
-                if(rex.data.length > 0) {
-                  that.searchWordData = rex.data;
-                  that.textBoxShow = true;
-                } else {
-                	that.textBoxShow = false;
-                  that.hint = "*No sensitive word was found";
-                }
-              }
-            }
-          })
-        } else {
-          this.textBoxShow = false;
-        }
-      },
-      addSensitive() {
-        var that = this;
-        that.textBoxShow = false;
-        $.ajax({
-          url: that.baseUrl + "addSensitiveWord",
-          type: "POST",
-          data: "{'word':'" + that.inputText + "'}",
-          headers: {
-            'Authorization': that.liangliang,
-          },
-          dataType: "json",
-          contentType: "application/json",
-          success: function(rex) {
-            if(rex.message == "*The sensitive word already exists") {
-              that.hint = "*The sensitive word already exists";
-              setTimeout(function() {
-                that.hint = "";
-              }, 3000)
-            }
-            if(rex.code == 200) {
-              $(".article-hint").css("color", "green");
-              that.hint = "Success";
-              setTimeout(function() {
-                $(".article-hint").css("color", "red");
-                that.hint = "";
-              }, 3000)
-            }
-          }
-        })
-      },
-      delSensitiveWord(wordId, sensitiveWord) {
-        var that = this;
-        delWordModal = new Modal({
-          "eleId": "delWordModal",
-          "title": "",
-          "content": "Are you sure you want to delete the sensitive word “" + sensitiveWord + "”",
-          success: function() {
-            $.ajax({
-              url: that.baseUrl + "delSensitiveWord",
-              type: "POST",
-              data: "{'wordId':'" + wordId + "'}",
-              headers: {
-                'Authorization': that.liangliang,
-              },
-              dataType: "json",
-              contentType: "application/json",
-              success: function(rex) {
-                if(rex.code == "200") {
-                  $("#delWordModal").hide();
-                  that.searchSensitive();
-                }
-              }
-            })
-          },
-          cancel: function() {}
-        });
-      }
-    },
-  }
+	import Vue from 'vue'
+	import { Modal } from '../../../static/js/modal.js'
+	/*测试编辑框*/
+	import VueKindEditor from 'vue-kindeditor'
+	import 'kindeditor/kindeditor-all-min.js'
+	import 'kindeditor/themes/default/default.css'
+	/*测试编辑框*/
+
+	Vue.use(VueKindEditor)
+	var delWordModal = "";
+	export default {
+		data() {
+			return {
+				liangliang: sessionStorage.getItem("liangliang").substring(0, sessionStorage.getItem("liangliang").length - 10),
+				baseUrl: sessionStorage.getItem("baseUrl"),
+				inputText: "",
+				textBoxShow: false,
+				searchWordData: [],
+				hint: "",
+				/*测试编辑框*/
+
+				editorText: '',
+				editorText2: '',
+				/*测试编辑框*/
+
+			}
+		},
+		methods: {
+			/*测试编辑框*/
+
+			onContentChange(val) {
+				this.editorText = val
+			},
+			onContentChange2(val) {
+				this.editorText2 = val
+			},
+			afterChange() {},
+			/*测试编辑框*/
+
+			searchSensitive() {
+				var that = this;
+				if(this.inputText) {
+					$.ajax({
+						url: that.baseUrl + "searchSensitiveWord",
+						type: "POST",
+						data: "{'word':'" + that.inputText + "'}",
+						headers: {
+							'Authorization': that.liangliang,
+						},
+						dataType: "json",
+						contentType: "application/json",
+						success: function(rex) {
+							if(rex.code == 200) {
+								if(rex.data.length > 0) {
+									that.searchWordData = rex.data;
+									that.textBoxShow = true;
+								} else {
+									that.textBoxShow = false;
+									that.hint = "*No sensitive word was found";
+								}
+							}
+						}
+					})
+				} else {
+					this.textBoxShow = false;
+				}
+			},
+			addSensitive() {
+				var that = this;
+				that.textBoxShow = false;
+				$.ajax({
+					url: that.baseUrl + "addSensitiveWord",
+					type: "POST",
+					data: "{'word':'" + that.inputText + "'}",
+					headers: {
+						'Authorization': that.liangliang,
+					},
+					dataType: "json",
+					contentType: "application/json",
+					success: function(rex) {
+						if(rex.message == "*The sensitive word already exists") {
+							that.hint = "*The sensitive word already exists";
+							setTimeout(function() {
+								that.hint = "";
+							}, 3000)
+						}
+						if(rex.code == 200) {
+							$(".article-hint").css("color", "green");
+							that.hint = "Success";
+							setTimeout(function() {
+								$(".article-hint").css("color", "red");
+								that.hint = "";
+							}, 3000)
+						}
+					}
+				})
+			},
+			delSensitiveWord(wordId, sensitiveWord) {
+				var that = this;
+				delWordModal = new Modal({
+					"eleId": "delWordModal",
+					"title": "",
+					"content": "Are you sure you want to delete the sensitive word “" + sensitiveWord + "”",
+					success: function() {
+						$.ajax({
+							url: that.baseUrl + "delSensitiveWord",
+							type: "POST",
+							data: "{'wordId':'" + wordId + "'}",
+							headers: {
+								'Authorization': that.liangliang,
+							},
+							dataType: "json",
+							contentType: "application/json",
+							success: function(rex) {
+								if(rex.code == "200") {
+									$("#delWordModal").hide();
+									that.searchSensitive();
+								}
+							}
+						})
+					},
+					cancel: function() {}
+				});
+			}
+		},
+	}
 </script>
 <style>
-  @import url("../../../static/css/base.css");
-  @import url("../../../static/css/sensitive.css");
+	@import url("../../../static/css/base.css");
+	@import url("../../../static/css/sensitive.css");
 </style>
---
+++ editor/src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="hello">
+		<quill-editor v-model="content" ref="myQuillEditor" :options="editorOption" @blur="onEditorBlur($event)" @focus="onEditorFocus($event)" @change="onEditorChange($event)">
+		</quill-editor>
+	</div>
+</template>
+<script>
+	import { quillEditor } from 'vue-quill-editor'
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				content:" ",
+				editorOption: {
+					toolbar: [
+              ['bold', 'italic', 'underline', 'strike'],
+              ['blockquote', 'code-block'],
+              [{ 'header': 1 }, { 'header': 2 }],
+              [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+              [{ 'script': 'sub' }, { 'script': 'super' }],
+              [{ 'indent': '-1' }, { 'indent': '+1' }],
+              [{ 'direction': 'rtl' }],
+              [{ 'size': ['small', false, 'large', 'huge'] }],
+              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+              [{ 'font': [] }],
+              [{ 'color': [] }, { 'background': [] }],
+              [{ 'align': [] }],
+              ['clean'],
+              ['link', 'image', 'video']
+            ],
+				}
+			}
+		},
+		methods: {
+			onEditorBlur() { //失去焦点事件
+			},
+			onEditorFocus() { //获得焦点事件
+			},
+			onEditorChange() { //内容改变事件
+				console.log(this.content)
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+	
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	
+	a {
+		color: #42b983;
+	}
+</style>
